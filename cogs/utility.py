2b6758650b9413240d29a8a606891d7c7e3eea84
Derek Olson <derek@derek-o.com>
2020-08-15 17:55:32 -0500

    add cewl utility function

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,10 +1,15 @@
 import collections
 import string
 import urllib.parse
+import urllib.request
 import random
+import re
+import operator
 import json
 import discord
 from discord.ext import commands
+from collections import Counter
+from bs4 import BeautifulSoup
 
 class Utility(commands.Cog):
 
@@ -64,6 +69,27 @@ class Utility(commands.Cog):
         
         if choice == 2:
             await ctx.send('tails')
+
+    @commands.command()
+    async def cewl(self, ctx: commands.Context, url, count: int = 100, word_len: str = "5-20", show_counts: bool = False):
+        body = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(body, 'html.parser')
+        tokens = soup.find(id="bodyContent").get_text(" ", strip=True).lower().split(" ")
+        nonPunct = re.compile('.*[A-Za-z].*')
+        raw_words = [w for w in tokens if nonPunct.match(w)]
+        min_len, max_len = [int(n) for n in word_len.split("-")]
+        raw_words = [word for word in raw_words if len(word) >= min_len and len(word) <= max_len and word.isalpha()]
+        raw_word_count = Counter(raw_words)
+
+        results = sorted(
+            raw_word_count.items(),
+            key=operator.itemgetter(1),
+            reverse=count > 0
+        )
+
+        results = results[:abs(count)]
+        results = "\n".join(f"{word} - {count}" if show_counts else word for word, count in results)
+        await ctx.send(f'```\n{results}```')
 
 def setup(bot):
     bot.add_cog(Utility(bot))
